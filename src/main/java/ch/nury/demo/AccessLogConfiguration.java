421a74f9643d01d59d7e5d5d27ab197cf6b63208
Julien Nury <jnury@users.noreply.github.com>
2019-05-26 00:23:55 +0200

    Initial Commit

---
+++ src/main/java/ch/nury/demo/AccessLogConfiguration.java
@@ -0,0 +1,26 @@
+package ch.nury.demo;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AccessLogConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer containerCustomizer() {
+        return container -> {
+            if (container instanceof TomcatServletWebServerFactory) {
+                ((TomcatServletWebServerFactory) container).addContextCustomizers((TomcatContextCustomizer) context -> {
+                    LogbackValve logbackValve = new LogbackValve();
+                    logbackValve.setFilename("logback-access.xml");
+                    context.getPipeline().addValve(logbackValve);
+                });
+            }
+        };
+    }
+}
